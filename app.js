d0d9139892f5e6b1ff5ae204ee98fe43ac35d464
blueoxygens <exboxer234@gmail.com>
2025-07-10 09:03:57 +0900

    [fix] contact me 수정

--- app.js
+++ app.js
@@ -62,3 +62,49 @@ function debounceUpdate() {
 inputs.forEach((input) => {
   input.addEventListener("input", debounceUpdate);
 });
+
+//좋아요 업데이트
+$(document).ready(function() {
+
+  const firebaseConfig = {
+    apiKey: "*",
+    authDomain: "ossmidterm.firebaseapp.com",
+    databaseURL: "https://ossmidterm-default-rtdb.firebaseio.com",
+    projectId: "ossmidterm",
+    storageBucket: "*",
+    messagingSenderId: "*",
+    appId: "*"
+  };
+
+  firebase.initializeApp(firebaseConfig);
+
+  const likesRef = firebase.database().ref('like');
+
+  function getLikeCount() {
+    likesRef.once('value', function(snapshot) {
+      const count = snapshot.val();
+      $("#update-num").text(count);
+    });
+  }
+
+  function increaseCount() {
+    likesRef.transaction(function(currentCount) {
+      return (currentCount || 0) + 1;
+    }, function(error, committed, snapshot) {
+      if (error) {
+        console.error("Transaction failed:", error);
+      } else if (!committed) {
+        console.log("Transaction aborted: Data could not be written to the database.");
+      } else {
+        console.log("Like count increased successfully.");
+        getLikeCount();
+      }
+    });
+  }
+
+  getLikeCount();
+
+  $(".contact-description").click(function() {
+    increaseCount();
+  });
+});
